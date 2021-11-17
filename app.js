36102baa0a06e45eb661a928fbd6e14354da9559
Prakash Dey <uslessid46@gmail.com>
2021-11-17 20:24:30 +0530

    updated

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let taskBtn = document.querySelector(".task-button");
 let modalCont = document.querySelector(".task-modal");
 let cancelBtn = document.querySelector(".cancel-btn");
+let priorityColor = document.querySelector(".priority-color");
 
 
 
 
 
 
+// ===========================Modal======================//
 taskBtn.addEventListener("click", (e) => {
     // Display modal
     console.log("Clicked on the button");
@@ -15,6 +17,11 @@ taskBtn.addEventListener("click", (e) => {
 })
 
 cancelBtn.addEventListener("click",(e)=>{
+    // hide modal
     modalCont.style.display = "none";
 })
 
+// Select priority
+priorityColor.addEventListener("click",(e)=>{
+    console.log(e);
+})
