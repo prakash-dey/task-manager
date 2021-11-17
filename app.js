ed25abb66f4cb5a3030e641c4c4704848674594a
Prakash Dey <uslessid46@gmail.com>
2021-11-17 19:32:05 +0530

    modal functionality implemented

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 let taskBtn = document.querySelector(".task-button");
+let modalCont = document.querySelector(".task-modal");
+let cancelBtn = document.querySelector(".cancel-btn");
 
 
 
 
 
-taskBtn.addEventListener("click",(e)=>{
+
+taskBtn.addEventListener("click", (e) => {
     // Display modal
     console.log("Clicked on the button");
+    modalCont.style.display = "flex";
+
+})
+
+cancelBtn.addEventListener("click",(e)=>{
+    modalCont.style.display = "none";
 })
