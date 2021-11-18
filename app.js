0bce7f57b23fec521f9c255d149149b56d48448b
Prakash Dey <uslessid46@gmail.com>
2021-11-18 23:17:41 +0530

    bug fixing and update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let taskBtn = document.querySelector(".task-button");
 let modalCont = document.querySelector(".task-modal");
 let cancelBtn = document.querySelector(".cancel-btn");
@@ -5,7 +6,9 @@ let priorityColor = document.querySelectorAll(".priority-color");
 let doneBtn = document.querySelector(".done-btn");
 let mainCont = document.querySelector(".task-cont");
 let textArea = document.querySelector(".text-area");
-
+let colors = ['red','yellow','blue','black'];
+let selectColor = colors[3];
+console.log(selectColor);
 
 
 
@@ -24,36 +27,36 @@ cancelBtn.addEventListener("click", (e) => {
 })
 
 // Select priority
-priorityColor.forEach(element => {
+priorityColor.forEach((element,idx) => {
     // priorityColor.forEach(element)
 
     element.addEventListener("click", (e) => {
         priorityColor.forEach(em => {
-            console.log("remove called");
-            console.log(em);
-
-            // em.target.classList.add("active");
             em.classList.remove("active");
 
         })
+        selectColor=colors[idx];
         e.target.classList.add("active");
+
     })
 });
 
 // ==========================Create tasks=======================//
 doneBtn.addEventListener("click", (e) =>{
-    createTask();
+    createTask(selectColor,textArea.value);
     modalCont.style.display = "none";
 
 })
 
-function createTask() {
+function createTask(selectColor,task) {
     let taskCont = document.createElement("div");
     taskCont.setAttribute("class", "task-item");
     taskCont.innerHTML = `<div class="priority-color-task">
                             </div>
                             <div class="task">
-                                ${textArea.value}
+                                ${task}
                             </div>`;
-    mainCont.appendChild(taskCont);
+    let priorityColor = document.querySelectorAll(".priority-color-task");
+    // priorityColor.style.backgroundColor = "black";
+    console.log(priorityColor);
 }
