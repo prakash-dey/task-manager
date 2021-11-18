5bf14090659734bc942c9cecdc5933124c25ad49
Prakash Dey <uslessid46@gmail.com>
2021-11-18 21:27:29 +0530

    bug fixing and updated

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ let taskBtn = document.querySelector(".task-button");
 let modalCont = document.querySelector(".task-modal");
 let cancelBtn = document.querySelector(".cancel-btn");
 let priorityColor = document.querySelectorAll(".priority-color");
-
+let doneBtn = document.querySelector(".done-btn");
+let mainCont = document.querySelector(".task-cont");
+let textArea = document.querySelector(".text-area");
 
 
 
@@ -16,7 +18,7 @@ taskBtn.addEventListener("click", (e) => {
 
 })
 
-cancelBtn.addEventListener("click",(e)=>{
+cancelBtn.addEventListener("click", (e) => {
     // hide modal
     modalCont.style.display = "none";
 })
@@ -25,14 +27,33 @@ cancelBtn.addEventListener("click",(e)=>{
 priorityColor.forEach(element => {
     // priorityColor.forEach(element)
 
-    element.addEventListener("click",(e)=>{
-        priorityColor.forEach(em=>{
+    element.addEventListener("click", (e) => {
+        priorityColor.forEach(em => {
             console.log("remove called");
-            em.target.classList.add("active");
-            em.target.classList.remove("active");
+            console.log(em);
+
+            // em.target.classList.add("active");
+            em.classList.remove("active");
 
         })
         e.target.classList.add("active");
-    })    
+    })
 });
 
+// ==========================Create tasks=======================//
+doneBtn.addEventListener("click", (e) =>{
+    createTask();
+    modalCont.style.display = "none";
+
+})
+
+function createTask() {
+    let taskCont = document.createElement("div");
+    taskCont.setAttribute("class", "task-item");
+    taskCont.innerHTML = `<div class="priority-color-task">
+                            </div>
+                            <div class="task">
+                                ${textArea.value}
+                            </div>`;
+    mainCont.appendChild(taskCont);
+}
