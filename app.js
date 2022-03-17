93b27e88be67febdfbcfc8c98c59dbc1b9f9a3de
Vaibhav 1 <vaibhav1@Vaibhavs-MacBook-Air-2.local>
2022-03-18 03:57:00 +0530

    Working on sorting

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-// =====Selectors======
+// =====Selectors====== 
 const taskBtn = document.querySelector(".task-button");
 const modalCont = document.querySelector(".task-modal");
 const cancelBtn = document.querySelector(".cancel-btn");
@@ -16,8 +16,8 @@ const taskItem = document.querySelectorAll(".task-item");
 // console.log(selectColor);
 
 // =====Global variables====
-let priority = ['high','medium','low','no-priority'];
-let selectPriority=priority[priority.length-1];
+let priority = ['high', 'medium', 'low', 'no-priority'];
+let selectPriority = priority[priority.length - 1];
 let lockStatus = true;
 let contentEdit = false;
 let taskArr = [];
@@ -35,7 +35,7 @@ cancelBtn.addEventListener("click", (e) => {
 })
 
 // Select priority
-priorityColor.forEach((element,idx) => {
+priorityColor.forEach((element, idx) => {
     // priorityColor.forEach(element)
 
     element.addEventListener("click", (e) => {
@@ -43,33 +43,31 @@ priorityColor.forEach((element,idx) => {
             em.classList.remove("active");
 
         })
-        selectPriority=priority[idx];
+        selectPriority = priority[idx];
         e.target.classList.add("active");
 
     })
 });
 
 // ==========================Create tasks=======================//
-doneBtn.addEventListener("click", (e) =>{
-    createTask(selectPriority,textArea.value);
-    setModalDefault()
-    
-
+doneBtn.addEventListener("click", (e) => {
+    createTask(selectPriority, textArea.value);
+    setModalDefault();
 })
 
-function setModalDefault(){
+function setModalDefault() {
     modalCont.style.display = "none";
-    textArea.value="";
-    selectPriority=priority[priority.length-1];
+    textArea.value = "";
+    selectPriority = priority[priority.length - 1];
     priorityColor.forEach(em => {
         em.classList.remove("active");
 
     })
-        
-    priorityColor[priorityColor.length-1].classList.add("active");
+
+    priorityColor[priorityColor.length - 1].classList.add("active");
 }
 
-function createTask(selectPriority,task,newTask = false) {
+function createTask(selectPriority, task, newTask = false) {
     let taskCont = document.createElement("div");
     taskCont.setAttribute("class", "task-item");
     taskCont.innerHTML = `
@@ -90,91 +88,94 @@ function createTask(selectPriority,task,newTask = false) {
                                 <div class="lock">
                                     <i class="fas fa-lock lock-icon"></i>
                                 </div>`;
-    if(newTask){
-        taskArr.push({selectPriority,task});
+    if (newTask) {
+        taskArr.push({ selectPriority, task });
     }
-  mainCont.appendChild(taskCont);
+    mainCont.appendChild(taskCont);
 }
 
 // ==================================Task Item==============================
 
 
 
-mainCont.addEventListener('click',(e)=>{
+mainCont.addEventListener('click', (e) => {
 
-    //==========lock unlock feature=================
+    //==========lock unlock feature=================//
 
-    if(e.target.classList.contains('lock-icon')){
+    if (e.target.classList.contains('lock-icon')) {
         console.log();
-        if(lockStatus){
+        if (lockStatus) {
             e.target.classList.add('fa-lock-open');
-            e.target.parentElement.previousElementSibling.style.display='flex';
+            e.target.parentElement.previousElementSibling.style.display = 'flex';
             lockStatus = false;
-        }else{
+        } else {
 
-        e.target.classList.remove('fa-lock-open')
-        console.log("lock Icon");
-        e.target.parentElement.previousElementSibling.style.display='none';
-        lockStatus = true;
-        e.target.parentElement.parentElement.children[1].setAttribute('contenteditable','false');
-    }}
+            e.target.classList.remove('fa-lock-open')
+            console.log("lock Icon");
+            e.target.parentElement.previousElementSibling.style.display = 'none';
+            lockStatus = true;
+            e.target.parentElement.parentElement.children[1].setAttribute('contenteditable', 'false');
+        }
+    }
 
     //================completed feature==================
 
-    else if(e.target.classList.contains('completed')){
-        e.target.parentElement.parentElement.style.display='none';
+    else if (e.target.classList.contains('completed')) {
+        e.target.parentElement.parentElement.style.display = 'none';
     }
-    
+
     // ============= Delete feature=====================
-    else if(e.target.classList.contains('delete')){
+    else if (e.target.classList.contains('delete')) {
         e.target.style.color = 'red';
-        setTimeout(()=>{
+        setTimeout(() => {
             e.target.parentElement.parentElement.remove();
-        },500);
+        }, 500);
 
     }
 
     //============= Edit feature========================
-    else if(e.target.classList.contains('edit')){
-        contentEdit=!contentEdit;
-        if(contentEdit){
+    else if (e.target.classList.contains('edit')) {
+        contentEdit = !contentEdit;
+        if (contentEdit) {
             e.target.style.color = 'red';
-            e.target.parentElement.previousElementSibling.setAttribute('contenteditable','true');
+            e.target.parentElement.previousElementSibling.setAttribute('contenteditable', 'true');
         }
-        else{
+        else {
             e.target.style.color = '';
-            e.target.parentElement.previousElementSibling.setAttribute('contenteditable','false');
+            e.target.parentElement.previousElementSibling.setAttribute('contenteditable', 'false');
 
         }
 
 
     }
     // =========== Color fetature
-    else if(e.target.classList.contains('color')){
+    else if (e.target.classList.contains('color')) {
         console.log('I am color');
         let color = e.target.classList[3];
         colorIndex = priority.indexOf(color);
-        if(colorIndex == 3){
-            colorIndex = -1 ;
+        if (colorIndex == 3) {
+            colorIndex = -1;
         }
         e.target.parentElement.parentElement.children[0].classList.remove(color);
         e.target.classList.remove(color);
-    
-        e.target.parentElement.parentElement.children[0].classList.add(priority[colorIndex+1]);
-        e.target.classList.add(priority[colorIndex+1]);
+
+        e.target.parentElement.parentElement.children[0].classList.add(priority[colorIndex + 1]);
+        e.target.classList.add(priority[colorIndex + 1]);
     }
 })
 
 //=============================== Sorting by color========================
-priorityContainer.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('high')){
-       let highTasks = mainCont.querySelectorAll(".high");
+priorityContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('high')) {
+        const highTasks = mainCont.querySelectorAll(".high");
     }
     console.log(taskItem.length)
     //removing the tickets first
-         for(let i=0; i<(taskItem.length);i++) {
-             console.log(i);
-             taskItem[i].remove();
-         } 
+    for (let i = 0; i < (taskItem.length); i++) {
+        console.log(i);
+        taskItem[i].remove();
+    }
+    console.log(mainCont.length);
+    console.log(taskArr.length);
 
 })
